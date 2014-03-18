f67ce492a0076bd06f024ccb5f0e77b17ccd6df5
Benjamin <armstrongmediahome@icloud.com>
2014-03-17 19:31:09 -0500

    Added res folder to Fix loading Errors
    Also made the Window size bigger, say thanks

--- src/com/mojang/ld22/Game.java
+++ src/com/mojang/ld22/Game.java
@@ -29,9 +29,9 @@ import com.mojang.ld22.screen.WonMenu;
 public class Game extends Canvas implements Runnable {
 	private static final long serialVersionUID = 1L;
 	private Random random = new Random();
-	public static final String NAME = "Minicraft";
-	public static final int HEIGHT = 120;
-	public static final int WIDTH = 160;
+	public static final String NAME = "MiniFarm";
+	public static final int HEIGHT = 340; //Used to be 120
+	public static final int WIDTH = 550; //Used to be 160
 	private static final int SCALE = 3;
 
 	private BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
